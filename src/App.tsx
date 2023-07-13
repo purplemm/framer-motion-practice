38d48a3877a71ba46a7a52ed317eda34cb2229cc
purplemm <boramm316@gmail.com>
2023-07-14 00:05:16 +0900

    Add useTransform

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import styled from "styled-components";
+import { motion, useMotionValue, useScroll, useTransform } from "framer-motion";
+
+const Wrapper = styled(motion.div)`
+  height: 200vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 200px;
+  height: 200px;
+  background-color: #fff;
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
+  const x =  useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const gradient = useTransform(x, [-800, 0, 800], [
+    "linear-gradient(135deg, rgb(0, 210, 238), rgb(0, 83, 238))",
+    "linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238))",
+    "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))"
+  ]);
+  const { scrollY, scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
+
+  useEffect(() => {
+    // x.onChange(() => console.log(x.get()));
+    // rotateZ.onChange(() => console.log(rotateZ.get()));
+    scrollY.onChange(() => {
+      console.log(scrollY.get(), scrollYProgress.get());
+    });
+  }, [scrollY, scrollYProgress]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper style={{background:gradient}}>
+      <Box style={{x, rotateZ, scale}} drag="x" dragSnapToOrigin />
+    </Wrapper>
   );
 }
 
